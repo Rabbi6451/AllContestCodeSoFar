b06d2df5aa18fe0e164d8fdc22d188497a81d664
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-16 01:25:57 +0600

    first

---
+++ UniversityContestCode1/MaximumNumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    long long count = 0;
+    cin>>a;
+    
+    vector<long long> t(a);
+    for(int i=0;i<a;i++){
+        cin>>t[i];
+    }
+    
+}
--- UniversityContestCode1/Summation.cpp
+++ UniversityContestCode1/Summation.cpp
@@ -1,7 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    int a;
-    
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    long long mx = arr[0];   
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > mx) {
+            mx = arr[i];
+        }
+    }
+
+    cout << mx;
+    return 0;
 }
