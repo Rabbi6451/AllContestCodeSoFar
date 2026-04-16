d6f4748115ea3cc5029e7b7cba6c52555138a5c7
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-17 00:21:18 +0600

    first

--- MyRandomCurisity/EvenNumberIndex.cpp
+++ MyRandomCurisity/EvenNumberIndex.cpp
@@ -7,19 +7,19 @@ int main() {
     cin >> n;
     
     vector<int> arr(n);
-    vector<int> evens;
     
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        if (arr[i] % 2 == 0) {
-            evens.push_back(arr[i]);
-        }
     }
     
-    for (int i = 0; i < evens.size(); i++) {
-        cout << i + 1;  
-        if (i != evens.size() - 1) {
-            cout << " ";
+    bool first = true;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            if (!first) {
+                cout << " ";
+            }
+            cout << i + 1;  
+            first = false;
         }
     }
     
