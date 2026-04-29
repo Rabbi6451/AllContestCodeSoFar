67c88eba05f4a1661807cb4f63296024b5557a1a
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-29 23:58:08 +0600

    second

---
+++ Practice3./SecondCode(3).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> coins(n);
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+        total += coins[i];
+    }
+    
+    sort(coins.rbegin(), coins.rend()); // sort descending
+    
+    int mySum = 0, count = 0;
+    for (int c : coins) {
+        mySum += c;
+        count++;
+        if (mySum > total - mySum) break;
+    }
+    
+    cout << count << "\n";
+    return 0;
+}
