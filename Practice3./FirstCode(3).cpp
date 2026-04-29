21937cc1410999e6cd1e2a6d285c44b0e666973e
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-29 23:56:38 +0600

    First

---
+++ Practice3./FirstCode(3).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    vector<int> nums;
+    
+    for (int i = 0; i < s.size(); i += 2) {
+        nums.push_back(s[i] - '0');  // extract digit
+    }
+    
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0) cout << "+";
+        cout << nums[i];
+    }
+    cout << "\n";
+    return 0;
+}
