ba94e12e33b135b031f3b9000b7781dc09d0d78c
mdrabbi <rabbihmgc2023@gmail.com>
2026-05-03 23:28:44 +0600

    third

--- CodeForceSolveAll/Num3.cpp
+++ CodeForceSolveAll/Num3.cpp
@@ -1,2 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int a;
+    cin>>a;
+    while (a--){
+        int b,c,d;
+        cin>>b>>c>>d;
+        if(b!=c && b!=d){
+            cout<<b<<endl;
+        }else if(c!=b && c!=d){
+            cout<<c<<endl;
+        }else if(d!=b && d!=c){
+            cout<<d<<endl;
+        }
+    }
+}
 
