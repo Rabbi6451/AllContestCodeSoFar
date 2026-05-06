dc88753d629daea3415db0a6788fcb6bcebb4767
mdrabbi <rabbihmgc2023@gmail.com>
2026-05-06 19:49:46 +0600

    third

--- CodeForceSolveAll/PracticeFirst.cpp
+++ CodeForceSolveAll/PracticeFirst.cpp
@@ -1,30 +1,18 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    int a;
-    cin>>a;
-    while(a--){
-        int b;
-        cin>>b;
-        vector<int> t(b);
-        for(int i=0;i<b;i++) cin>>t[i];
+int main ()
+{
+  vector<int> myvector;
+  int myint;
 
-        vector<int> div6, div3,div2,others;
-        for(int x : t){
-            if(x%6==0) div6.push_back(x);
-            else if(x%2==0) div2.push_back(x);
-            else if(x%3==0) div3.push_back(x);
-            else others.push_back(x);
-        }
-        vector<int> result;
-        result.insert(result.end(), div6.begin(), div6.end());
-        result.insert(result.end(), div2.begin(), div2.end());
-        result.insert(result.end(), others.begin(), others.end());
-        result.insert(result.end(), div3.begin(), div3.end());
-        for(int i=0;i<b;i++){
-            cout<<result[i]<<(i+1==b ? '\n' : ' ');
-        }
-    }
+  cout << "Please enter some integers (enter 0 to end):\n";
+
+  do {
+    cin >> myint;
+    myvector.push_back (myint);
+  } while (myint);
+
+  cout << "myvector stores " << int(myvector.size()) << " numbers.\n";
 }
