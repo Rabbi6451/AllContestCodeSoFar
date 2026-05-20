e811d718236037ee1bc13289ba7950dc9b1f0c2d
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-05-21 00:02:26 +0600

    first

---
+++ CodeForceSolve2/2Num3MaxValue.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    while(a--){
+        int b;
+        cin>>b;
+        vector<int> t(b);
+        for(int i=0;i<b;i++){
+            cin>>t[i];
+        }
+            auto max_it = std::max_element(t.begin(), t.end());
+             int max_val = *max_it;
+            cout<<max_val*b<<endl;
+    }
+}
