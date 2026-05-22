4c1ada1c7915c436ea52b5c4f5e035bff1377dac
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-05-23 01:02:12 +0600

    first

--- CodeForceSolve2/2Num7.cpp
+++ CodeForceSolve2/2Num7.cpp
@@ -1,16 +1,22 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    int a;
-    cin>>a;
-    while(a--){
-        int b,c;
-        cin>>b>>c;
-        vector<long long> t(b);
-        for(int i=0;i<b;i++) cin>>t[i];
+int main() {
 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        if (k == 1) {
+            if (is_sorted(a.begin(), a.end())) cout << "YES\n";
+            else cout << "NO\n";
+        } else {
+            cout << "YES\n";
+        }
     }
-    
-
 }
