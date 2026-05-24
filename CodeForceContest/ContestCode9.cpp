d49683afa2a126ec5d03df133da7effb4a0cba7a
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-05-24 22:57:50 +0600

    first

---
+++ CodeForceContest/ContestCode9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int mn = INT_MAX, mx = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int a; cin >> a;
+            mn = min(mn, a);
+            mx = max(mx, a);
+        }
+        cout << (mx - mn + 1) / 2 << "\n";
+    }
+}
