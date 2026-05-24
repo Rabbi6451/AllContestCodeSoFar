1e7f822317ea2b7fc186a0b4f1f4a79ce7191e25
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-05-25 01:08:05 +0600

    First

---
+++ CodeForceSolve2/2Num10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(auto &x:a) cin>>x;
+        bool sorted=true;
+        for(int i=0;i<n-1;i++)
+            if(a[i]>a[i+1]){sorted=false;break;}
+        if(sorted) cout<<n<<"\n";
+        else cout<<1<<"\n";
+    }
+}
