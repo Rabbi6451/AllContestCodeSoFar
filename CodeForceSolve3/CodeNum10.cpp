f38ebfbe4ca500817493dd5f17e2fa3cff3551e4
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-06-08 23:58:29 +0600

    second

---
+++ CodeForceSolve3/CodeNum10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+long long digit_sum(long long n) {
+    long long s = 0;
+    while (n > 0) {
+        s = s + n % 10;
+        n /= 10;
+    }
+    return s;
+}
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x;
+        cin >> x;
+        int count = 0;
+        for (long long y = x + 1; y <= x + 90; y++) {
+            if (y - digit_sum(y) == x) {
+                count++;
+            }
+        }
+        cout << count << "\n";
+    }
+    return 0;
+}
