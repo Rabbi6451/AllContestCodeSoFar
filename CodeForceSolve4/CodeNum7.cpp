55b828ef746abfe6acf289d1f43d44589603e682
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-06-13 17:01:45 +0600

    second

---
+++ CodeForceSolve4/CodeNum7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < k; i++) {
+        int onesCount = 0;
+        for (int j = i; j < n; j += k) {
+            if (s[j] == '1') onesCount++;
+        }
+        if (onesCount % 2 != 0) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+   
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
