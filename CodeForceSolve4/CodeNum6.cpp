96f167dc55d697bf967f66d9994c37bcf0105759
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-06-13 15:45:14 +0600

    first

---
+++ CodeForceSolve4/CodeNum6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    while(a--){
+        int b;
+        cin>>b;
+        vector<int> t(b);
+        for(int i=0;i<b;i++){
+            cin>>t[i];
+        }
+        int maximum = *max_element(t.begin(),t.end());
+        int minimum = *min_element(t.begin(),t.end());
+
+        cout<<(maximum-minimum)+1<<endl;
+
+    }
+}
