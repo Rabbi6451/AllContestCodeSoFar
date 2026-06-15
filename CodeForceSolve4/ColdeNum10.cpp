866713d9e266a1a056c8031a6a9bc269f7b57af6
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-06-15 19:35:24 +0600

    first

---
+++ CodeForceSolve4/ColdeNum10.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        for (int i = 0; i < n; i++) cin >> b[i];
+
+        int totalDiff = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] > b[i]) {
+                totalDiff += (a[i] - b[i]);
+            }
+        }
+
+        cout << totalDiff + 1 << "\n";
+    }
+}
