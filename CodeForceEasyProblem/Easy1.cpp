e48ba2b1c334cfac2129acd2af7c656f2dfb8dc6
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-06-16 12:49:10 +0600

    first

---
+++ CodeForceEasyProblem/Easy1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    int b,c;
+    char k;
+    while(a--){
+        cin>>b>>k>>c;
+        cout<<b+c<<endl;
+    }    
+    
+}
