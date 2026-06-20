e109e03aff664639d521c3a36cbad6a7f0c1d33a
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-06-20 17:28:11 +0600

    first

---
+++ CodeForceEasyProblem/Easy8.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char grid[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    bool symmetric = true;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i][j] != grid[2-i][2-j]) {
+                symmetric = false;
+            }
+        }
+    }
+
+    cout << (symmetric ? "YES" : "NO") << endl;
+}
---
+++ CodeForceEasyProblem/Easy9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    while(a--){
+        string c;
+        cin>>c;
+        
+    }
+}
