96192e4c27d61c5b9ef3ac54c1563096afeea0c0
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-07-08 17:41:11 +0600

    first

--- CodeForceSolve7/CodeNum9.cpp
+++ CodeForceSolve7/CodeNum9.cpp
@@ -9,28 +9,16 @@ int main() {
         cin >> n;
         string s;
         cin >> s;
-        
-        int unanswered = 0;
-        bool ok = true;
-        
-        for (char ch : s) {
-            if (ch == 'Q') {
-                unanswered++;
+
+        int open = 0; 
+        for (char x : s) {
+            if (x == 'Q') {
+                open++;
             } else {
-                if (unanswered > 0) {
-                    unanswered--;
-                } else {
-                    ok = false;
-                    break;
-                }
+                if (open > 0) open--; 
+    
             }
         }
-        
-        if (ok && unanswered == 0) {
-            cout << "Yes" << endl;
-        } else {
-            cout << "No" << endl;
-        }
+        cout << (open == 0 ? "Yes" : "No") << "\n";
     }
-    return 0;
 }
