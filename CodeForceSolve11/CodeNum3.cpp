cc4168f0de4a27625427d0e699a88c6956648086
Rabbi6451 <rabbihmgc2023@gmail.com>
2026-08-07 00:23:15 +0600

    first

--- CodeForceSolve11/CodeNum3.cpp
+++ CodeForceSolve11/CodeNum3.cpp
@@ -4,28 +4,30 @@
 using namespace std;
 
 void solve(){
-    int b;
-    cin >> b;
-    vector<int> t(b);
-    for(int i = 0; i < b; i++){
-        cin >> t[i];
+    int n;
+    cin >> n;
+    vector<int> w(n);
+    for (int i = 0; i < n; i++) cin >> w[i];
+
+    if (n % 2 != 0) {
+        cout << "NO\n";
+        return;
     }
-    int mx = 0;
-    for(int i = 0; i < b - 1; i++){
-        int result = abs(t[i] - t[i+1]);
-        mx = max(mx, result);
+
+    int maxEven = INT_MIN; 
+    int minOdd  = INT_MAX; 
+
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) minOdd  = min(minOdd, w[i]);  
+        else            maxEven = max(maxEven, w[i]); 
     }
-    if(mx > 1){
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
-    }
+
+    if (minOdd - maxEven >= 2) cout << "YES\n";
+    else cout << "NO\n";
 }
 
 int main(){
-    int a;
-    cin >> a;
-    while(a--){
-        solve();
-    }
+    int t;
+    cin >> t;
+    while (t--) solve();
 }
